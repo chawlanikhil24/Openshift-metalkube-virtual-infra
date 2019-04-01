e476cf621e0c96f48c7a712b2039fd0762dfa77c
Nikhil Chawla <nichawla@redhat.com>
2019-04-01 17:56:09 +0530

    Added README and fixes

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Openshift-metalkube-virtual-infra
 This repository has the scripts that automates the deployment of virtual machines that simulates bare-metal environment for Ironic.
+
+MetalKube Installer Dev Scripts
+===============================
+
+This set of scripts configures some libvirt VMs and associated
+[virtualbmc](https://docs.openstack.org/tripleo-docs/latest/install/environments/virtualbmc.html) processes to enable deploying to them as dummy baremetal nodes.
+
+This is very similar to how we do TripleO testing so we reuse some roles
+from tripleo-quickstart here.
+
+# Pre-requisites
+
+- CentOS 7.5 or greater (installed from 7.4 or newer)
+- file system that supports d_type (see Troubleshooting section for more information)
+- ideally on a bare metal host
+- run as a user with passwordless sudo access
+- get a valid pull secret (json string) from https://cloud.openshift.com/clusters/install#pull-secret
+
+# Instructions
+
+## Configuration
+
+Make a copy of the `config_example.sh` to `config_$USER.sh`, and set the
+`PULL_SECRET` variable to the secret obtained from cloud.openshift.com.
+
+For baremetal test setups where you don't require the VM fake-baremetal nodes,
+you may also set `NODES_FILE` to reference a manually created json file with
+the node details (see [ironic_hosts.json.example](ironic_hosts.json.example)),
+and `NODES_PLATFORM` which can be set to e.g "baremetal" to disable the libvirt
+master/worker node setup. See [common.sh](common.sh) for other variables that
+can be overridden.
--- VM_automation.sh
+++ VM_automation.sh
@@ -4,9 +4,9 @@ VM_COUNT=${1}
 VM_COUNT="${VM_COUNT:-3}"
 echo "$VM_COUNT"
 
-
-#sudo yum install git python-virtualenv  -y
+sudo yum install git python34-virtualenv  -y
 virtualenv-3 venv
+
 source venv/bin/activate
 pip3 install jinja2
 python3 script.py ${VM_COUNT}
@@ -15,11 +15,13 @@ python3 script.py ${VM_COUNT}
 git clone https://github.com/openshift-metalkube/dev-scripts
 rm -rf dev-scripts/tripleo-quickstart-config/metalkube-nodes.yml
 mv out.yaml dev-scripts/tripleo-quickstart-config/metalkube-nodes.yml
+mv config_$USER.sh dev-scripts/
 cd dev-scripts
 
 
-##assuming the install to be new install
-./01_install_requirements.sh
+##Uncomment this if this is a fresh install
+#./01_install_requirements.sh
+
 ./02_configure_host.sh
 
 # Verify vms are present
@@ -34,7 +36,7 @@ export OS_URL=http://localhost:6385/
 #Ensure No baremetal is running
 openstack baremetal node list
 
-for count in $(seq(0 $VM_COUNT));do
+for count in $(seq 0 $VM_COUNT );do
 	openstack baremetal node create \
 	  --name openshift_master_${count} \
 	  --driver ipmi \
@@ -44,14 +46,18 @@ for count in $(seq(0 $VM_COUNT));do
 	  --driver-info ipmi_port=$(( 6230 + count ))
 done
 
-for count in $(seq(0 $VM_COUNT));do
-        openstack baremetal node manage openshift_master_${count}
-done
-
 echo "Wait for 60 seconds"
 for i in $(seq 0 60);do
     echo "Waiting for nodes to come UP..."
     sleep 1
 done
 
+for count in $(seq 0 $VM_COUNT );do
+	echo "Trying to manage node: "$count
+	openstack baremetal node manage openshift_master_${count}
+	sleep 1
+done
+
+
 openstack baremetal node list 
+
