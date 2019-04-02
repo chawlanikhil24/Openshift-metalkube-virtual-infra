437290c0c6a1dc6a4a3bcb932230ebc8670097a8
Nikhil Chawla <nichawla@redhat.com>
2019-04-02 11:06:47 +0530

    bug fix

--- VM_automation.sh
+++ VM_automation.sh
@@ -49,8 +49,10 @@ for count in $(seq 0 $VM_COUNT );do
 done
 
 echo "Wait for 60 seconds"
+total_time_left=60
 for i in $(seq 0 60);do
-    echo "Waiting for nodes to come UP..."
+    echo "Waiting for nodes to come UP."${total_time_left}" Seconds left..."
+    total_time_left=$(( total_time_left - i ))
     sleep 1
 done
 
